745e0a61d018d58cf26d729df74bca066efe5ec7
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-07-30 10:44:49 -0300

    retrive data from video

--- analyzing_video.py
+++ analyzing_video.py
@@ -9,3 +9,9 @@ def get_frames(filename):
     video = cv2.VideoCapture(filename)
     while video.isOpened():
         ret, frame = video.read()
+        if ret:
+            yield frame
+        else:
+            break
+    video.release()
+    yield None
